142242d78aad014ec53cf8b8980c3a98f3eb5325
Jakkrit Junrat <ball_vong_yo_gh@hotmail.com>
2019-02-15 10:25:04 +0700

    mongoose

--- app.js
+++ app.js
@@ -3,27 +3,27 @@ var express = require('express');
 var path = require('path');
 var bodyParser = require('body-parser');
 var session = require('express-session');
-var passport = require('passport');
-var auth0 = require('passport-auth0');
+// var passport = require('passport');
+// var auth0 = require('passport-auth0');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var strategy = new auth0({
-  domain: 'dev-dcs-trace.auth0.com',
-  clientID: 'H57kkWkUvGWedmSF-pLkoDK6F478WgWT',
-  clientSecret: 'SoiH8uvnUEMt0bb58FajVVzk5jJactmo3CXKlAFAeL1da93_Qezmm9YigKaVKH_a',
-  callbackURL: 'http://localhost:3000/callback'
-}, function(accessToken, refreshToken, extraParam, profile, done) {
-  return done(null, profile);
-});
+// var strategy = new auth0({
+//   domain: 'dev-dcs-trace.auth0.com',
+//   clientID: 'H57kkWkUvGWedmSF-pLkoDK6F478WgWT',
+//   clientSecret: 'SoiH8uvnUEMt0bb58FajVVzk5jJactmo3CXKlAFAeL1da93_Qezmm9YigKaVKH_a',
+//   callbackURL: 'http://localhost:3000/callback'
+// }, function(accessToken, refreshToken, extraParam, profile, done) {
+//   return done(null, profile);
+// });
 
-passport.use(strategy);
-passport.serializeUser(function(user, done) {
-  done(null, user);
-});
-passport.deserializeUser(function (user, done) {
-  done(null, user);
-});
+// passport.use(strategy);
+// passport.serializeUser(function(user, done) {
+//   done(null, user);
+// });
+// passport.deserializeUser(function (user, done) {
+//   done(null, user);
+// });
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -39,12 +39,12 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use(session({
   secret: 'dcs.work',
-  resave: true,
+  resave: false,
   saveUninitialized: true
 }));
 
-app.use(passport.initialize());
-app.use(passport.session());
+// app.use(passport.initialize());
+// app.use(passport.session());
 
 app.use(function (req, res, next) {
   res.locals.loggedIn = false;
@@ -65,27 +65,27 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
-app.get('/login', passport.authenticate('auth0', {
-  clientID: 'H57kkWkUvGWedmSF-pLkoDK6F478WgWT',
-  domain: 'dev-dcs-trace.auth0.com',
-  redirectUri: 'http://localhost:3000/callback',
-  responseType: 'code',
-  audience: 'https://dev-dcs-trace.auth0.com/userinfo',
-  scope: 'openid, profile'
-}), function (req, res) {
-  res.redirect('/');
-});
+// app.get('/login', passport.authenticate('auth0', {
+//   clientID: 'H57kkWkUvGWedmSF-pLkoDK6F478WgWT',
+//   domain: 'dev-dcs-trace.auth0.com',
+//   redirectUri: 'http://localhost:3000/callback',
+//   responseType: 'code',
+//   audience: 'https://dev-dcs-trace.auth0.com/userinfo',
+//   scope: 'openid, profile'
+// }), function (req, res) {
+//   res.redirect('/');
+// });
 
-app.get('/logout', function (req, res) {
-  req.logout();
-  res.redirect('/');
-});
+// app.get('/logout', function (req, res) {
+//   req.logout();
+//   res.redirect('/');
+// });
 
-app.get('/callback', passport.authenticate('auth0', {
-  failureRedirect: '/error'
-}), function (req, res) {
-  res.redirect('/user');
-});
+// app.get('/callback', passport.authenticate('auth0', {
+//   failureRedirect: '/error'
+// }), function (req, res) {
+//   res.redirect('/user');
+// });
 
 app.get('/user', function (req, res, next) {
   res.render('user', {
